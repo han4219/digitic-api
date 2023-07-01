2dffc72c539148bd331dbba4fe48ed669d564d18
han4219 <han157589@gmail.com>
2023-07-01 22:25:35 +0700

    init project

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+app.listen(PORT, () => {
+  console.log('Server running at port', PORT)
+})
