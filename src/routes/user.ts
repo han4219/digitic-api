faffc41f4007e8fb26c107f2ac086f904a90ac58
han4219 <han157589@gmail.com>
2023-07-16 08:49:28 +0700

    separate users router for admin panel

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -16,14 +16,15 @@ import {
 
 export default (router: Router) => {
   router.get('/user/me', auth, getMe)
-  router.put('/user/me', auth, updateMe)
-  router.get('/user/wishlist', auth, getWishList)
-  router.get('/users', auth, isAdmin, getAllUsers)
-  router.put('/user/update-address', auth, updateAddress)
+  router.put('/user/me/update', auth, updateMe)
+  router.get('/user/wishlists', auth, getWishList)
+  router.put('/user/address/update', auth, updateAddress)
 
-  router.get('/user/:id', auth, isAdmin, getUser)
-  router.put('/user/:id', auth, isAdmin, updateUser)
-  router.delete('/user/:id', auth, isAdmin, deleteUser)
-  router.put('/user/activate/:id', auth, isAdmin, activateUser)
-  router.put('/user/deactivate/:id', auth, isAdmin, deactivateUser)
+  // Admin router
+  router.get('/admin/user/:id', auth, isAdmin, getUser)
+  router.get('/admin/users', auth, isAdmin, getAllUsers)
+  router.put('/admin/user/update/:id', auth, isAdmin, updateUser)
+  router.delete('/admin/user/delete/:id', auth, isAdmin, deleteUser)
+  router.put('/admin/user/activate/:id', auth, isAdmin, activateUser)
+  router.put('/admin/user/deactivate/:id', auth, isAdmin, deactivateUser)
 }
